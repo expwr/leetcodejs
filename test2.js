ec71b0c399bd8d45f96ad97d09ef10f18199b5bb
Noah Diana <noah.diana10312005@gmail.com>
2023-07-01 16:09:46 -0400

    test2

---
+++ test2.js
@@ -0,0 +1,14 @@
+const maxSubarraySum = (arr) => {
+    let currentSum = arr[0];
+    let maxSum = arr[0];
+  
+    for (let i = 1; i < arr.length; i++) {
+      currentSum = Math.max(arr[i], currentSum + arr[i]);
+      maxSum = Math.max(maxSum, currentSum);
+    }
+  
+    return maxSum;
+  };
+
+const arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+console.log(maxSubarraySum(arr));
