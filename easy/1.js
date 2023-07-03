139c289951b4687c70857081b0bcaebfd1c3bf27
Noah Diana <noah.diana10312005@gmail.com>
2023-07-03 17:40:34 -0400

    now added comments and title for all

--- easy/1.js
+++ easy/1.js
@@ -1,14 +1,14 @@
 // 1. Two Sum
 
 var twoSum = function(nums, target) {
-    let map = new Map();
+    let map = new Map(); // Create a new Map to store values and their indices
 
     for (let i = 0; i < nums.length; i++) {
-        if (map.has(nums[i])) {
-            return [map.get(nums[i]), i];
+        if (map.has(nums[i])) { // If the complement of the current number exists in the Map
+            return [map.get(nums[i]), i]; // Return the indices of the complement and the current number
         } else {
-            let b = target - nums[i];
-            map.set(b, i)
+            let b = target - nums[i]; // Calculate the complement of the current number
+            map.set(b, i); // Store the complement and its index in the Map
         }
     }
 };
